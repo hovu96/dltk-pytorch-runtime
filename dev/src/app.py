4fe500baf15ef45f2bf4973d79d6e34c85b2c269
hovu96 <hovu96>
2020-02-15 19:38:16 +0000

    uploadign and downloading notebook

--- dev/src/app.py
+++ dev/src/app.py
@@ -5,6 +5,11 @@ import os
 # parameters: #https://github.com/jupyter/notebook/blob/master/notebook/notebookapp.py
 
 
+notebook_name = "Algo.ipynb"
+notebook_dir = os.getenv('NOTEBOOK_PATH', "/notebooks")
+notebook_file = os.path.join(notebook_dir, notebook_name)
+
+
 class FitHandler(tornado.web.RequestHandler):
     def post(self):
         self.write("fit")
@@ -15,11 +20,24 @@ class ApplyHandler(tornado.web.RequestHandler):
         self.write("apply")
 
 
+class NotebookHandler(tornado.web.RequestHandler):
+    def get(self):
+        with open(notebook_file, 'r') as f:
+            source = f.read()
+        self.write(source)
+
+    def put(self):
+        source = self.request.body
+        with open(notebook_file, "w") as f:
+            f.write(source)
+
+
 class PytorchApp(jupyterlab_server.LabServerApp):
     def start(self):
         self.web_app.add_handlers('.*$', [
             (r"/_dltk/fit", FitHandler),
             (r"/_dltk/apply", ApplyHandler),
+            (r"/_dltk/notebook", NotebookHandler),
         ])
         super().start()
 
@@ -28,9 +46,9 @@ if __name__ == "__main__":
     PytorchApp.launch_instance(
         port=os.getenv('PORT', 8888),
         open_browser=False,
-        notebook_dir=os.getenv('NOTEBOOK_PATH', "/notebooks"),
+        notebook_dir=notebook_dir,
         allow_root=True,
-        default_url="/tree",
+        default_url="/notebooks/%s" % notebook_name,
         # base_url='/jupyterlab/',
         # debug=False,
         token="",
